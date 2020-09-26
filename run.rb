ad524b3399db6d4e22cda8f5b035faba4b47cda8
swiknaba <swiknaba@users.noreply.github.com>
2020-09-26 19:54:02 +0200

    we cannot cd from within a Ruby script..

--- run.rb
+++ run.rb
@@ -6,17 +6,18 @@ app_name = ARGV[0] || Kernel.gets.chomp
 puts 'Enter name of the git repository, hit enter to use the app name'
 repo_name = ARGV[1] || Kernel.gets.chomp || app_name
 
-system("mkdir #{repo_name}; cd #{repo_name}")
+system("mkdir #{repo_name}")
 
 # checkout default repo, re-initialize git
-puts "cloning #{DEFAULT_APP_GIT_URL}.."
-system("git clone #{DEFAULT_APP_GIT_URL}")
+puts "cloning #{DEFAULT_APP_GIT_URL} to #{repo_name}"
+system("git clone #{DEFAULT_APP_GIT_URL} #{repo_name}")
 
 puts 'initializing new repository'
-system('rm -rf .git')
-system('git init')
+system("rm -rf #{repo_name}/.git")
+system("git init #{repo_name}")
 
 puts "renaming the app to '#{app_name}'"
+# todo
 
 # OR execute the script via CURL instead of keeping a local copy
 puts 'removing this script'
